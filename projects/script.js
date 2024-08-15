e57a1bc67bcef453c7a4bb3cb18c4d4e13a7fd09
Sathish1504 <sathishkumar150401@gmail.com>
2024-08-15 10:20:12 +0530

    first commit

--- projects/script.js
+++ projects/script.js
@@ -20,12 +20,12 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Jigar Sable";
+            document.title = "Projects | Portfolio Sathish Kumar";
             $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
             document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "/assets/images/favhand.png");
+            $("#favicon").attr("href", "/assets/images/profile.jpg");
         }
     });
 
